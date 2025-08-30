2c2722b740540ff0000cf76cde4f36ab8fd5ff2f
Sean Standen <standen.sean@gmail.com>
2025-08-30 12:55:04 -0700

    types created.

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,3 +10,39 @@ export type PageInfo = {
     description: string;
     featuredImage: ImageProps;
 }
+
+export type SocialLink = {
+    linkURL: string;
+    icon: ImageProps;
+}
+export type FreelanceLink = {
+    calloutText: string;
+    linkText: string;
+    linkURL: string;
+}
+
+
+export type SkillsTechnologies = {
+    title: string;
+    content: any;
+}
+
+export type Project = {
+    name: string;
+    websiteURL: string;
+    technologies: SkillsTechnologies[];
+    description: any;
+    featuredImage: ImageProps;
+}
+
+export type Job = {
+    companyName: string;
+    jobTitle: string;
+    startDate: Date | string;
+    endDate: string;
+    description: any;
+    websiteURL: string;
+    responsibilities: any;
+    projects: Project[];
+    featuredImage: ImageProps;
+}
