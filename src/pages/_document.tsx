0d255ad0248e1558f901d6630c4af99e45c1d872
Sean Standen <standen.sean@gmail.com>
2025-08-30 14:33:41 -0700

    Fonts loaded and configured.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // Import the font variable class
+  // Note: This import must be at the top of the file
+  // import { satoshiRegular } from '../lib/fonts';
+  // But since _document.tsx is a custom document, we need to require it here
+  const satoshiRegular = require('../lib/fonts').satoshiRegular;
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={satoshiRegular.variable}>
         <Main />
         <NextScript />
       </body>
