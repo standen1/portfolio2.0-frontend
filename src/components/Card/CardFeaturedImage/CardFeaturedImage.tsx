bee6726e45d13c7a05dbe4b38b0f73a291ac161c
Sean Standen <standen.sean@gmail.com>
2025-09-16 23:36:22 -0700

    CardFeaturedImage error on build.

--- src/components/Card/CardFeaturedImage/CardFeaturedImage.tsx
+++ src/components/Card/CardFeaturedImage/CardFeaturedImage.tsx
@@ -3,16 +3,20 @@ import { CardFeaturedImageProps } from "./CardFeaturedImageProps"
 import styles from "./CardFeaturedImage.module.css";
 
 const CardFeaturedImage: React.FC<CardFeaturedImageProps> = ({ image }) => {
-    return (
-        <div className={styles.CardFeaturedImage}>
-            <img
-                src={image.src}
-                alt={image.alt}
-                width={image.width}
-                height={image.height}
-            />
-        </div>
-    );
+    if ( image ) {
+        return  (
+            <div className={styles.CardFeaturedImage}>
+                <img
+                    src={image.src}
+                    alt={image.alt}
+                    width={image.width}
+                    height={image.height}
+                />
+            </div> 
+        )
+    } else {
+        return (<></>);
+    }
 }
 
 export default CardFeaturedImage;
