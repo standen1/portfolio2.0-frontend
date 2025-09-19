a9e036a3b536db08383e3e3fda03f1666561a39a
Sean Standen <standen.sean@gmail.com>
2025-09-19 10:55:05 -0700

    Construction component and portfolio page temp with construction component.

---
+++ src/components/lib/UnderConstruction/UnderConstruction.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './UnderConstruction.module.css';
+
+const UnderConstruction: React.FC = () => {
+  return (
+    <div className={styles.UnderConstruction}>
+      <h1>Under Construction</h1>
+      <p>This page is currently under construction. Please check back later.</p>
+    </div>
+  );
+};
+
+export default UnderConstruction;
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,21 @@
+import UnderConstruction from "@/components/lib/UnderConstruction/UnderConstruction";
+import Head from "next/head";
+
+export default function Contact() {
+
+  return (
+    <>
+      <Head>
+        <title>Sean Standen | Contact</title>
+        {/* ... (meta tags) */}
+      </Head>
+      <div>
+        <main style={{ backgroundColor: "#F8F8F8"}}>
+          <section id="under-construction">
+            <UnderConstruction />
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}
